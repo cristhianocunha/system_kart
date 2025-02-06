b779921c112845b6556a5b17d484ef9823aa926e
Cristhiano Cunha <cristhiano.cunha@gmail.com>
2025-02-06 00:54:00 -0300

    Corrigindo problema do layouts guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,7 +19,7 @@
     </head>
     <body>
         <div class="font-sans text-gray-900 dark:text-gray-100 antialiased">
-        @yield('content')
+        {{ $slot }}
         </div>
 
         @livewireScripts
