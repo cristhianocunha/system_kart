357d4b584137ba6ba079a2ecdc96da0b790a1602
cristhianocunha <cristhiano.cunha@gmail.com>
2025-02-22 09:36:29 -0300

    add botoao corridas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,18 +7,16 @@
             Olá, Piloto {{ Auth::user()->name }} 👋
         </h2>
     </x-slot>
-
-
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <h1 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                Acesse as corridas separadas. </h1>
             <div class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 <ul>
                     @foreach($corridas as $corrida)
                     <li>
-                        <a
-                            class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"
-                            href="{{ route('bateria01.index', ['corrida' => $corrida->corrida] ) }}">Corrida {{ $corrida->corrida }}</a>
+                        <button class="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600"
+                            href="{{ route('bateria01.index', ['corrida' => $corrida->corrida] ) }}">Corrida {{ $corrida->corrida }}</button>
                     </li>
 
                     @endforeach
@@ -31,7 +29,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                    Melhor volta: {{ $staticUser->TVM }}
+                    Sua Melhor volta: {{ $staticUser->TVM }}
                 </h2>
             </div>
         </div>
