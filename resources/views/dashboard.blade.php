f91cf146ac1fc95cd9ceb70d00114ff105dcc9f9
Cristhiano Cunha <cristhiano.cunha@gmail.com>
2025-05-15 09:24:58 -0300

    fix link corridas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,14 +9,13 @@
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <h1 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Acesse as corridas separadas. </h1>
             <div class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 <ul>
                     @foreach($corridas as $corrida)
                     <li>
-                        <button class="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600"
-                            href="{{ route('bateria01.index', ['corrida' => $corrida->corrida] ) }}">Corrida {{ $corrida->corrida }}</button>
+                        <a
+                            class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"
+                            href="{{ route('bateria01.index', ['corrida' => $corrida->corrida] ) }}">Corrida {{ $corrida->corrida }}</a>
                     </li>
 
                     @endforeach
